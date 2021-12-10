d53def6462abbb9ee4957608bbf416228c76dad1
matias acuna <agroang@gmail.com>
2021-12-10 13:04:14 +0900

    added type.js plugin, still need to check more about it and use partials for it

---
+++ source/assets/javascripts/_type.js
@@ -0,0 +1,14 @@
+// typed.js plugin stuff
+// import Typed from 'typed.js';
+
+// const options = {
+//   strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+//   typeSpeed: 40
+// };
+
+// const typed = new Typed('.element', options);
+
+// const targetDiv = document.getElementById('test');
+
+// // typed.js plugin stuff
+// export { options, typed, targetDiv };
